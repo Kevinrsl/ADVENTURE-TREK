21d680718d7cedb75d08b85818faf7f71436c92b
kevin <kevin.roussel.22800@gmail.com>
2022-06-08 15:47:35 +0200

    ajout des fichiers

---
+++ islande.js
@@ -0,0 +1,24 @@
+// var total = 0;
+// function clic() {
+//   total = total + 1;
+//   document.getElementById("compteur").textContent = total;
+// }
+
+// var boutonElement = document.getElementById("voyage");
+// boutonElement.addEventListener("click", clic);
+
+// function ouvrirFermerSpoiler(bouton) {
+//   var divContenu = bouton.nextSibling;
+//   if (divContenu.nodeType == 3) divContenu = divContenu.nextSibling;
+//   if (divContenu.style.display == "block") {
+//     divContenu.style.display = "none";
+//   } else {
+//     divContenu.style.display = "block";
+//   }
+// }
+
+// function afficherMasquer(1) {
+//   if (document.getElementById(1).style.display == "none")
+//     document.getElementById(1).style.display = "block";
+//   else document.getElementById(1).style.display = "none";
+// }
